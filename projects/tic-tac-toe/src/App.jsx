4079254f3e35b557cf498eecef8be9ddfa851d65
thelastnicko <thelastnicko@gmail.com>
2023-11-21 01:53:02 -0500

    add emojis and test game for winner and draw

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,147 +1,67 @@
 import { useState } from "react";
 import confetti from "canvas-confetti";
-
-
-const TURNS = {
-  X: "x",
-  O: "o",
-};
-
-
-const Square = ({children, isSelected, updateBoard, index}) =>{
-  
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-  
-  const handleClick = () => {
-updateBoard(index)
-  }
-  return(
-    <div  onClick={handleClick} className={className}>
-    {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+import { Square } from "./components/Square";
+import { TURNS } from "./constants";
+import { checkWinnerFrom, checkEndGame } from "./logic/board";
+import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  );
+  const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(TURNS.X);
   // Null means no winner, false means is a draw.
   const [winner, setWinner] = useState(null);
 
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo;
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a];
-      }
-    }
-    // If no winner.
-    return null;
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
   };
 
-  const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
-  }
-
-  const checkEndGame = (newBoard) => {
-return newBoard.every((square) => square !== null)
-  }
+ 
 
   const updateBoard = (index) => {
-
     // If position is selected, do not update.
-    if(board[index] || winner)return
+    if (board[index] || winner) return;
     // Update board.
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
     // Switch turns.
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
     // Check for a winner.
-    const newWinner = checkWinner(newBoard)
-    if(newWinner) {
-      confetti()
-      setWinner(newWinner)
-    }else if (checkEndGame(newBoard)) {
-      setWinner(false)
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
-  }
+  };
 
   return (
     <main className="board">
       <h1>Tic tac toe ⚔️</h1>
       <button onClick={resetGame}>Reset game</button>
       <section className="game">
-        {
-          board.map((square, index) => {
-           return(
-            <Square
-            key={index}
-            index={index}
-            updateBoard={updateBoard}
-            >
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
               {square}
-
             </Square>
-           )
-          })
-        }
+          );
+        })}
       </section>
 
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
-
       </section>
 
-   
-        {
-          winner !== null && (
-            <section className="winner">
-              <div className="text">
-                <h2>
-                  {
-                    winner === false
-                    ? 'Draw!'
-                    : 'Winner:'
-                  }
-                </h2>
-                <header className="win">
-                  {winner && <Square>{winner}</Square>}
-                </header>
-                <footer>
-                  <button onClick={resetGame}>Play Again</button>
-                </footer>
-
-              </div>
-            </section>
-          )
-        }
-     
-
+    <WinnerModal resetGame={resetGame} winner={winner}/>
     </main>
-  )
+  );
 }
 
 export default App;
