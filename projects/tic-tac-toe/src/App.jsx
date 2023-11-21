399f77e502f82a7d8b3013c6f25894748a863bf2
thelastnicko <thelastnicko@gmail.com>
2023-11-21 00:17:57 -0500

    added final setup for end of the game

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -21,22 +21,57 @@ updateBoard(index)
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  );
 
   const [turn, setTurn] = useState(TURNS.X);
+  // Null means no winner, false means is a draw.
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    // If no winner.
+    return null;
+  };
 
   const updateBoard = (index) => {
-    
+
     // If position is selected, do not update.
-    if(board[index])return
+    if(board[index] || winner)return
+    // Update board.
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    // Switch turns.
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-
     setTurn(newTurn)
+    // Check for a winner.
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+    }
   }
 
   return (
